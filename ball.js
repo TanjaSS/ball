f32285fc70626ecaddf4470e44539245970f5fc2
Tanja <stricevictanja@gmail.com>
2013-11-14 21:58:47 +0100

    Ball stops

--- ball.js
+++ ball.js
@@ -1,16 +1,22 @@
-var g = 400;
-var t = 0;
+var gravity = 400;
+var time = 0;
+var height = $(window).height();
+var ballHeight;
 
 function moveBall() {
-    var s = g * t * t / 2;
-    $("#ball").css("margin-top", s);
+    var position = gravity * time * time / 2;
+    
+    if(position + ballHeight < height) {
+        $("#ball").css("margin-top", position);
+    }
 }
 
 function updateTime() {
-    t = t + 0.01;
+    time = time + 0.01;
     moveBall();
 }
 
 $(function() {
+    ballHeight = $("#ball").height();
     window.setInterval(updateTime, 10);
 });
